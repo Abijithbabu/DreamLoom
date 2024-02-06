35b745d5936789d7cea821f095c10fd4b10556cf
Abhijith Babu <abijithb2002@gmail.com>
2024-02-06 10:05:19 +0530

    Initalization commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import BG from './assets/images/landing.jpg';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={BG} style={styles.container}>
+        <Text style={styles.title}>DreamLoom</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#27a0A1',
+    fontSize: 50,
+    fontWeight: 'bold',
+    borderWidth: 2,
+    borderColor: '#000',
+    padding: 10
   },
 });
